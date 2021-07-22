0e3bbf0ae8863c5398c4f19b80f480a6937c481d
Ruan Berte <berte.ruan@gmail.com>
2021-07-22 16:37:13 -0300

    Test if throw an error when invalid input is provided

--- modulo-1/lib/calculator.js
+++ modulo-1/lib/calculator.js
@@ -1,3 +1,10 @@
 module.exports.sum = (sum1, sum2) => {
-    return sum1 + sum2
+    const int1 = parseInt(sum1, 10)
+    const int2 = parseInt(sum2, 10)
+
+    if (Number.isNaN(int1) || Number.isNaN(int2)) {
+        throw new Error('Please check your input')
+    } 
+
+    return int1 + int2
 }
