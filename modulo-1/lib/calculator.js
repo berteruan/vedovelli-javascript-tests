b41d5d4fd1ac9a5e68f3e7c1c015c275b3194abf
Ruan Berte <berte.ruan@gmail.com>
2021-07-23 11:03:15 -0300

    Add prettier configuration and format files

--- modulo-1/lib/calculator.js
+++ modulo-1/lib/calculator.js
@@ -1,10 +1,10 @@
 module.exports.sum = (sum1, sum2) => {
-    const int1 = parseInt(sum1, 10)
-    const int2 = parseInt(sum2, 10)
+  const int1 = parseInt(sum1, 10);
+  const int2 = parseInt(sum2, 10);
 
-    if (Number.isNaN(int1) || Number.isNaN(int2)) {
-        throw new Error('Please check your input')
-    } 
+  if (Number.isNaN(int1) || Number.isNaN(int2)) {
+    throw new Error('Please check your input');
+  }
 
-    return int1 + int2
-}
+  return int1 + int2;
+};
--- modulo-1/lib/queryString.spec.js
+++ modulo-1/lib/queryString.spec.js
@@ -1,13 +1,13 @@
-const { queryString } = require("./queryString");
+const { queryString } = require('./queryString');
 
-describe("Object to query string", () => {
-  it("should create a valid query string when an object is provided", () => {
+describe('Object to query string', () => {
+  it('should create a valid query string when an object is provided', () => {
     const obj = {
-      name: "Ruan",
-      profession: "developer",
+      name: 'Ruan',
+      profession: 'developer',
     };
 
-    expect(queryString(obj)).toBe("name=Ruan&profession=developer");
+    expect(queryString(obj)).toBe('name=Ruan&profession=developer');
   });
 });
 
