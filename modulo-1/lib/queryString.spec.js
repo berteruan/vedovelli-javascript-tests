87257caface8837057f6ec679cd01d794fe87caa
Ruan Berte <berte.ruan@gmail.com>
2021-07-23 11:14:31 -0300

    Add test cases and implementations

--- modulo-1/lib/queryString.spec.js
+++ modulo-1/lib/queryString.spec.js
@@ -9,6 +9,28 @@ describe('Object to query string', () => {
 
     expect(queryString(obj)).toBe('name=Ruan&profession=developer');
   });
+
+  it('should create a valid query string even when an array is provided', () => {
+    const obj = {
+      name: 'Ruan',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe('name=Ruan&abilities=JS,TDD');
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Ruan',
+      abilities: {
+        JS: 'test',
+      },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
 });
 
 // describe('Query string to object', () => {
